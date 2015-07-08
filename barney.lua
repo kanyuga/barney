2951264340bc0fb4eb9d1d50ae028d457a11f246
kanyuga <me@topher.co.ke>
2015-07-08 07:52:04 +0300

    Added lua variant

---
+++ barney.lua
@@ -0,0 +1,30 @@
+isSad = true
+
+SadBarney = {}
+
+function SadBarney:new(o)
+	local self = {};
+	self.stop = function()
+		print ("I stop being sad")
+	end
+	return self
+end
+
+function sad()
+	if isSad then
+		isSad = false
+		print("When I get sad,")
+		return true
+	else
+		return SadBarney:new()
+	end
+end
+
+function beAwesome()
+	print("and be awesome instead")
+end
+
+if sad() then
+	sad().stop()
+	beAwesome()
+end
