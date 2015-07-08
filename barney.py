5ec4240f0165769ccf960472aac393d7f149e847
kanyuga <me@topher.co.ke>
2015-07-08 07:51:40 +0300

    Added python variant

---
+++ barney.py
@@ -0,0 +1,21 @@
+isSad = True
+
+class SadBarney:
+	def stop(self):
+		print "I stop being sad"
+
+def sad():
+    global isSad
+    if (isSad):
+        isSad = False
+        print "When I get sad,"
+        return True
+    else:
+        return SadBarney()
+
+def beAwesome():
+    print "and be awesome instead"
+
+if (sad() == True): 
+    sad().stop()
+    beAwesome()
