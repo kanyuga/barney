d0a3d6a29b6655014bcf94edde5ce83c1518d3cc
kanyuga <me@topher.co.ke>
2015-07-08 07:51:11 +0300

    Added php variant

---
+++ barney.php
@@ -0,0 +1,27 @@
+<?php
+$isSad = true;
+
+class SadBarney {
+    public function stop() {
+        echo "I stop being sad,\n";
+    }
+}
+
+function sad() {
+    global $isSad;
+    if ($isSad) {
+        $isSad = false;
+    echo "When I get sad\n";
+        return true;
+    } 
+    else return new SadBarney();
+}
+
+function beAwesome() {
+    echo "and be awesome instead\n";
+}
+
+if (sad() === true) {
+    sad()->stop();
+    beAwesome();
+}
